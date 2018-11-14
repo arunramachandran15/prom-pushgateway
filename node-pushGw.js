890b83221990b605d7862cb28edbcf0e8e9b07e6
Arun <arunramachandran15@users.noreply.github.com>
2018-11-14 15:15:03 +0530

    Update node-pushGw.js

--- node-pushGw.js
+++ node-pushGw.js
@@ -1,5 +1,5 @@
 const client = require('prom-client');
-let gateway = new client.Pushgateway('http://ab4af2835e68411e8a73a064fb26ff25-1814932884.us-west-2.elb.amazonaws.com:9091/');
+let gateway = new client.Pushgateway('http://pushgw_url:9091/');
 const collectDefaultMetrics = client.collectDefaultMetrics;
 collectDefaultMetrics({ timeout: 1000 });
 collectDefaultMetrics({ prefix: 'cronmetrics' });
